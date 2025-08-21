09f10573de7d6ff5460e9094d2f1e9b57817cf82
Tasneem-123 <tsnmfkhan@gmail.com>
2025-08-21 10:08:27 +0530

    script.js

--- script.js
+++ script.js
@@ -1,99 +1,61 @@
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
-const slider = document.querySelector('.slider'); 
-const images = document.querySelectorAll('.image')
+const slider = document.querySelector(".slider");
+const leftArrow = document.querySelector(".arrow.left");
+const rightArrow = document.querySelector(".arrow.right");
+const dots = document.querySelectorAll(".button");
+const images = document.querySelectorAll(".image");
 
-let slideNumber = 1;
-const length = images.length;
+let index = 0;
+let autoPlayInterval;
 
-//start for dots
+// Function to update slider
+function updateSlider() {
+  slider.style.transform = `translateX(-${index * 100}%)`;
 
-const bottom = document.querySelector('.bottom');
+  // Update active dot
+  dots.forEach((dot, i) => {
+    dot.classList.toggle("active", i === index);
+  });
+}
 
-for(let i=0; i<length; i++){
-    const div = document.createElement('div');
-    div.className = 'button';
-    bottom.appendChild(div);
-};
+// Move left
+function moveLeft() {
+  index = (index - 1 + images.length) % images.length;
+  updateSlider();
+  resetAutoPlay();
+}
 
-const buttons = document.querySelectorAll('.button');
+// Move right
+function moveRight() {
+  index = (index + 1) % images.length;
+  updateSlider();
+  resetAutoPlay();
+}
 
-buttons[0].style.backgroundColor = 'white';
-
-const resetBg = ()=>{
-    buttons.forEach((button) =>{
-        button.style.backgroundColor = 'transparent';
-        button.addEventListener('mouseover', stopSlideShow);
-        button.addEventListener('mouseout', startSlideShow);
-    });
-};
-
-buttons.forEach((button, i) =>{
-    button.addEventListener('click',()=>{
-        resetBg();
-        slider.style.transform = `translateX(-${i*800}px)`;
-        slideNumber = i + 1;
-        button.style.backgroundColor = 'white';
-    });
+// Dot navigation
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    index = i;
+    updateSlider();
+    resetAutoPlay();
+  });
 });
 
+// Auto-play
+function startAutoPlay() {
+  autoPlayInterval = setInterval(() => {
+    moveRight();
+  }, 3000); // change image every 3s
+}
 
+function resetAutoPlay() {
+  clearInterval(autoPlayInterval);
+  startAutoPlay();
+}
 
+// Event listeners
+leftArrow.addEventListener("click", moveLeft);
+rightArrow.addEventListener("click", moveRight);
 
-const changecolor = ()=>{
-    resetBg();
-    buttons[slideNumber-1].style.backgroundColor = 'white';
-};
-
-//end dots
-
-
-const nextSlide = ()=>{
- slider.style.transform = `translateX(-${slideNumber*800}px)`;
-    slideNumber++;
-};
-const prevSlide = ()=>{
- slider.style.transform = `translateX(-${(slideNumber-2)*800}px)`;
-    slideNumber--;
-};
-const getFirstSlide = ()=>{
- slider.style.transform = `translateX(0px)`;
-    slideNumber = 1;
-};
-const getLastSlide = ()=>{
- slider.style.transform = `translateX(-${(length-1)*800}px)`;
-    slideNumber = length;
-};
-
-right.addEventListener('click',()=>{
-    slideNumber < length ? nextSlide() : getFirstSlide();
-    changecolor();
-});
-left.addEventListener('click',()=>{
-    slideNumber > 1 ? prevSlide() : getLastSlide(); 
-    changecolor();
-});
-
-// Start Auto Slider
-
-let slideInterval;
-
-const startSlideShow = () =>{
-slideInterval = setInterval(()=>{
-slideNumber < length ? nextSlide() : getFirstSlide();
-changecolor();
-},3000);
-};
-
-const stopSlideShow = ()=>{
-    clearInterval(slideInterval);
-};
-
-startSlideShow();
-
-slider.addEventListener('mouseover', stopSlideShow);
-slider.addEventListener('mouseout', startSlideShow);
-right.addEventListener('mouseover', stopSlideShow);
-right.addEventListener('mouseout', startSlideShow);
-left.addEventListener('mouseover', stopSlideShow);
-left.addEventListener('mouseout', startSlideShow);
+// Init
+updateSlider();
+startAutoPlay();
